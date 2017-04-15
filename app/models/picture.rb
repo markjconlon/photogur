92225db47511d1221b5ab6de211408898ee4d763
Mark <markjconlon@gmail.com>
2017-04-15 19:51:43 -0400

    added validations, still unsure how to show them to screen

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,9 @@
 class Picture < ApplicationRecord
+  validates :artist, presence: true
+  validates :title, length: { in: 3..20,
+    wrong_length: "Your title must be between %{count}" }
+  validates :url, presence: true, uniqueness: true
+
+  # @picture.valid?
+  # @picture.errors.messages
 end
